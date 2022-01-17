21f5a79a3f6c1a8e6603fe24fa59b5a47aa4b3e2
DDe3 <lasalinasb@uce.edu.ec>
2022-01-17 08:09:11 -0500

    taller lunes 17

---
+++ src/components/Contador.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Contador</h2>
+  <p>{{contador}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <button v-on:click="aumentarContador">Aumentar</button>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+export default {
+    name: "Contador",
+    data() {
+        return {
+            contador: 5,
+        }
+    },
+    methods: {
+        obtenerCuadrado() {
+            console.log("Se calcula el cuadrado")
+            return this.contador*this.contador
+        },
+        aumentarContador() {
+            this.contador++
+        }
+    },
+    computed: {
+        obtenerCuadradoComputado() {
+            console.log("Se calcula el cuadrado")
+            return this.contador*this.contador
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
